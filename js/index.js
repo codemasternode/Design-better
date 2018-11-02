8a19fe26a9b0997ba9f6f0f104c64c38fe458965
codemasternode <marcinwarzybok@outlook.com>
2018-11-02 13:21:14 +0100

    add some style to index page

--- js/index.js
+++
@@ -1,7 +0,0 @@
-let secondImages = document.querySelectorAll('.second__image')
-console.log(secondImages)
-secondImages.forEach((image,index) => {
-    image.style.top = (50 * index) + 'px'
-    image.style.left = (30 * (index)) + '%'
-    console.log(image.style)
-});
